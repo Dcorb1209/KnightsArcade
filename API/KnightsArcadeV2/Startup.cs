f9e8cb81de54ffb4706b2b3ef1254190b073a758
Yitzak Hernandez <kre3ed@gmail.com>
2019-02-28 15:40:37 -0500

    Added working knights arcade databse api

---
+++ API/KnightsArcadeV2/Startup.cs
@@ -0,0 +1,68 @@
+﻿using KnightsArcadeV2.Infrastructure.Data;
+using KnightsArcadeV2.Infrastructure.Data.Interface;
+using KnightsArcadeV2.Infrastructure.Logic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace KnightsArcadeV2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<RDSLogic>().AddScoped<IRDSData, RDSData>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "KnightsArcade Database API",
+                    Description = "An ASP.NET Core Web API for the KnightsArcade Amazon RDS MySql Database.",
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "KnightsArcade Database API");
+                c.RoutePrefix = "swagger";
+            });
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
